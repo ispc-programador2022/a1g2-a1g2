85a0fb307b067014937175a320e4ec1bf107a797
OmarMiranda <OmarAlfredoMiranda@gmail.com>
2022-06-26 19:35:46 -0300

    sprint2

--- Funciones/genrnd.py
+++ Funciones/genrnd.py
@@ -1,6 +1,1 @@
-from random import randint
-
-def genrnd():
-    x = [randint(0,100) for i in range(50)]
-    return x
-print(genrnd())
+#
--- Funciones/genrndsum.py
+++ Funciones/genrndsum.py
@@ -1,9 +1,1 @@
-from genrnd import genrnd
-def genrndsum():
-    x = genrnd()
-    suma = 0
-    for i in x:
-        suma += i
-    return suma
-
-print(genrndsum())
+#
